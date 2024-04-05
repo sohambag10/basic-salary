d6fc8d2c3c0e3abaec073529e499b654f6821038
sohambag10 <166116757+sohambag10@users.noreply.github.com>
2024-04-05 19:42:13 +0530

    Add files via upload

---
+++ sum of all natural numbers.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   int sum=0;
+   for(int i=1;i<=10;i++){
+    sum=sum+i;
+    cout<<"num is:"<<i<<endl;;
+   }
+   cout<<"the sum of numbers is:"<<sum;
+    return 0;
+
+}
+
